8c2abe0f13591297c5df229e5fd859d0edd56ae5
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-11-04 11:50:02 +0000

    Try to find chezmoi when installing

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 set -e # -e: exit on error
 
+bin_dir="$HOME/.local/bin"
 if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
-  chezmoi="$bin_dir/chezmoi"
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
@@ -13,11 +12,15 @@ if [ ! "$(command -v chezmoi)" ]; then
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
-else
-  chezmoi=chezmoi
+fi
+
+# check if PATH contains $bin_dir
+if ! echo "$PATH" | grep -q "$bin_dir"; then
+  export PATH="$bin_dir:$PATH"
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+
+exec chezmoi init --apply "--source=$script_dir"
