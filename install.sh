b39ca9e0d282486e1585c1f81dfdf0c1705a8196
ekroon <123574+ekroon@users.noreply.github.com>
2024-10-17 21:21:22 +0200

    Migrate to nix home-manager

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
-
 set -e # -e: exit on error
 
-bin_dir="$HOME/.local/bin"
-if [ ! "$(command -v chezmoi)" ]; then
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsLS https://chezmoi.io/get)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+if [ ! "$(command -v curl)" ]; then
+  echo "Needs curl to install"
+  exit 1
 fi
 
-# check if PATH contains $bin_dir
-if ! echo "$PATH" | grep -q "$bin_dir"; then
-  export PATH="$bin_dir:$PATH"
-fi
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+source /home/vscode/.nix-profile/etc/profile.d/nix.sh
+
+nix shell 'nixpkgs#acl' --command bash -c 'sudo env PATH=$PATH setfacl -k /tmp'
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
+
+ln -s "${script_dir}/nix" /home/vscode/.config/home-manager
+
+nix run /home/vscode/.config/home-manager/#homeConfigurations."vscode".activationPackage
 
 exec chezmoi init --apply "--source=$script_dir"
