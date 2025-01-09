1abae2792039c6956a2856cf0cada4a5a0cd039b
ekroon <123574+ekroon@users.noreply.github.com>
2025-01-08 23:27:27 -0800

    Switch to mise, remove NIX

--- install.sh
+++ install.sh
@@ -6,27 +6,11 @@ if [ ! "$(command -v curl)" ]; then
   exit 1
 fi
 
-sh <(curl -L https://nixos.org/nix/install) --no-daemon --no-modify-profile
-
-source /home/vscode/.nix-profile/etc/profile.d/nix.sh
-
-mkdir -p $HOME/.config/nix 2>/dev/null
-echo "experimental-features = nix-command flakes fetch-closure" > $HOME/.config/nix/nix.conf
-
-# nix shell 'nixpkgs#acl.bin' --command bash -c 'sudo env PATH=$PATH setfacl -k /tmp'
-
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-# install devbox
-export FORCE=1
-curl -fsSL https://get.jetify.com/devbox | bash
-unset FORCE
 
-echo 'eval "$(devbox global shellenv)"' >> $HOME/.bashrc
-. $HOME/.bashrc
-
-devbox global add acl atuin chezmoi starship tailscale
-eval "$(devbox global shellenv --preserve-path-stack -r)" && hash -r
+curl https://mise.run | sh
+~/.local/bin/mise use --global aqua:atuinsh/atuin chezmoi starship aqua:tailscale/tailscale
 
 exec chezmoi init --apply "--source=$script_dir"
