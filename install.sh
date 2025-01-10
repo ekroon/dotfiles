17db6467c2207440d5a206328c17cd64e2b02544
ekroon <123574+ekroon@users.noreply.github.com>
2025-01-10 05:12:48 -0800

    Add fzf

--- install.sh
+++ install.sh
@@ -9,9 +9,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-
 curl https://mise.run | sh
-~/.local/bin/mise use --global aqua:atuinsh/atuin chezmoi starship aqua:tailscale/tailscale
+~/.local/bin/mise use --global aqua:atuinsh/atuin chezmoi starship aqua:tailscale/tailscale fzf
 
 eval "$(~/.local/bin/mise activate bash)"
 exec $(~/.local/bin/mise which chezmoi) init --apply "--source=$script_dir"
