51e649de706f06af2974911209e6b7bd27619546
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2025-02-09 21:33:21 +0100

    config darkmode

--- root/private_dot_config/nvim/lua/plugins/core.lua
+++ root/private_dot_config/nvim/lua/plugins/core.lua
@@ -1,6 +1,30 @@
+local function get_darkmode_setting()
+  local handle = io.popen("curl -s http://localhost:5000/apple-interface-style")
+  if not handle then
+    return "light"
+  end
+  local result = handle:read("*a")
+  handle:close()
+
+  local success, json = pcall(vim.fn.json_decode, result)
+  if success and json.AppleInterfaceStyle == "Dark" then
+    return "dark"
+  else
+    return "light"
+  end
+end
+
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin-latte",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      fallback = get_darkmode_setting(),
+    },
   },
 }
