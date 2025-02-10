14daee18309e6845211fd487e10f01de3d489ec6
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2025-02-10 12:04:44 +0100

    Dark mode only

--- root/dot_wezterm.lua
+++ root/dot_wezterm.lua
@@ -1,15 +1,32 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+	-- if wezterm.gui then
+	-- 	return wezterm.gui.get_appearance()
+	-- end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "catppuccin-mocha"
+	else
+		return "catppuccin-latte"
+	end
+end
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = "catppuccin-latte"
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.font = wezterm.font("SauceCodePro Nerd Font", { weight = "Regular" })
-config.font_size = 14
+config.font_size = 13
 config.bold_brightens_ansi_colors = "BrightAndBold"
 
 config.window_decorations = "RESIZE"
--- root/private_dot_config/nvim/lua/plugins/core.lua
+++ root/private_dot_config/nvim/lua/plugins/core.lua
@@ -1,17 +1,18 @@
 local function get_darkmode_setting()
-  local handle = io.popen("curl -s http://localhost:5000/apple-interface-style")
-  if not handle then
-    return "light"
-  end
-  local result = handle:read("*a")
-  handle:close()
-
-  local success, json = pcall(vim.fn.json_decode, result)
-  if success and json.AppleInterfaceStyle == "Dark" then
-    return "dark"
-  else
-    return "light"
-  end
+  -- local handle = io.popen("curl -s http://localhost:5000/apple-interface-style")
+  -- if not handle then
+  --   return "light"
+  -- end
+  -- local result = handle:read("*a")
+  -- handle:close()
+  --
+  -- local success, json = pcall(vim.fn.json_decode, result)
+  -- if success and json.AppleInterfaceStyle == "Dark" then
+  --   return "dark"
+  -- else
+  --   return "light"
+  -- end
+  return "dark"
 end
 
 return {
