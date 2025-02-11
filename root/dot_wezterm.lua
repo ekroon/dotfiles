05945ea2112e1e34ae194aeffe58df10918ea6b3
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2025-02-11 15:54:20 +0100

    Update .wezterm.lua

--- root/dot_wezterm.lua
+++ root/dot_wezterm.lua
@@ -32,7 +32,7 @@ config.bold_brightens_ansi_colors = "BrightAndBold"
 config.window_decorations = "RESIZE"
 
 local act = wezterm.action
-config.keys = {
+local keys = {
 	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
 	{ key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") },
 	-- Make Option-Right equivalent to Alt-f; forward-word
@@ -40,14 +40,132 @@ config.keys = {
 	{
 		key = "9",
 		mods = "ALT",
-		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS|WORKSPACES" }),
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS|WORKSPACES|TABS" }),
+	},
+	{ key = "r", mods = "CTRL|SHIFT", action = wezterm.action.ReloadConfiguration },
+}
+
+config.mux_env_remove = {}
+
+local home_dir = os.getenv("HOME")
+wezterm.add_to_config_reload_watch_list(home_dir .. "/.ssh/config")
+wezterm.add_to_config_reload_watch_list(home_dir .. "/.ssh/codespaces")
+config.ssh_domains = wezterm.default_ssh_domains()
+
+config.unix_domains = {
+	{
+		name = "permanent",
 	},
 }
 
-wezterm.add_to_config_reload_watch_list("~/.ssh/config")
-wezterm.add_to_config_reload_watch_list("~/.ssh/codespaces")
+config.default_gui_startup_args = { "connect", "permanent" }
 
-config.ssh_domains = wezterm.default_ssh_domains()
+wezterm.on("update-right-status", function(window, pane)
+	-- Each element holds the text for a cell in a "powerline" style << fade
+	local cells = {}
 
+	table.insert(cells, (pane and pane:get_domain_name()) or "<none>")
+
+	-- Figure out the cwd and host of the current pane.
+	-- This will pick up the hostname for the remote host if your
+	-- shell is using OSC 7 on the remote host.
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		local cwd = ""
+		local hostname = ""
+
+		if type(cwd_uri) == "userdata" then
+			-- Running on a newer version of wezterm and we have
+			-- a URL object here, making this simple!
+
+			cwd = cwd_uri.file_path
+			hostname = cwd_uri.host or wezterm.hostname()
+		else
+			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+			-- which doesn't have the Url object
+			cwd_uri = cwd_uri:sub(8)
+			local slash = cwd_uri:find("/")
+			if slash then
+				hostname = cwd_uri:sub(1, slash - 1)
+				-- and extract the cwd from the uri, decoding %-encoding
+				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+					return string.char(tonumber(hex, 16))
+				end)
+			end
+		end
+
+		-- Remove the domain name portion of the hostname
+		local dot = hostname:find("[.]")
+		if dot then
+			hostname = hostname:sub(1, dot - 1)
+		end
+		if hostname == "" then
+			hostname = wezterm.hostname()
+		end
+
+		table.insert(cells, cwd)
+		table.insert(cells, hostname)
+	end
+
+	-- I like my date/time in this style: "Wed Mar 3 08:14"
+	local workspace = "#" .. window:active_workspace()
+	table.insert(cells, workspace)
+
+	-- An entry for each battery (typically 0 or 1 battery)
+	for _, b in ipairs(wezterm.battery_info()) do
+		table.insert(cells, string.format("%.0f%%", b.state_of_charge * 100))
+	end
+
+	-- The powerline < symbol
+	local LEFT_ARROW = utf8.char(0xe0b3)
+	-- The filled in variant of the < symbol
+	local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+	-- Color palette for the backgrounds of each cell
+	local colors = {
+		"#3c1361",
+		"#52307c",
+		"#663a82",
+		"#7c5295",
+		"#b491c8",
+	}
+
+	-- Foreground color for the text across the fade
+	local text_fg = "#c0c0c0"
+
+	-- The elements to be formatted
+	local elements = {}
+	-- How many cells have been formatted
+	local num_cells = 0
+
+	-- Translate a cell into elements
+	local function push(text, is_last)
+		local cell_no = num_cells + 1
+		table.insert(elements, { Foreground = { Color = text_fg } })
+		table.insert(elements, { Background = { Color = colors[cell_no] } })
+		table.insert(elements, { Text = " " .. text .. " " })
+		if not is_last then
+			table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+			table.insert(elements, { Text = SOLID_LEFT_ARROW })
+		end
+		num_cells = num_cells + 1
+	end
+
+	while #cells > 0 do
+		local cell = table.remove(cells, 1)
+		push(cell, #cells == 0)
+	end
+
+	window:set_right_status(wezterm.format(elements))
+end)
+
+table.insert(keys, { key = "t", mods = "CTRL|SHIFT", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) })
+table.insert(keys, { key = "c", mods = "CTRL|SHIFT", action = act.SwitchToWorkspace({ name = "codespaces" }) })
+table.insert(keys, { key = "d", mods = "CTRL|SHIFT", action = act.SwitchToWorkspace({ name = "default" }) })
+table.insert(keys, { key = "[", mods = "CTRL|SHIFT", action = act.SwitchWorkspaceRelative(1) })
+table.insert(keys, { key = "]", mods = "CTRL|SHIFT", action = act.SwitchWorkspaceRelative(-1) })
+
+--assign keys to config
+config.keys = keys
 -- and finally, return the configuration to wezterm
 return config
